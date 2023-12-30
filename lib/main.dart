da5d1bbc4b70bb6d2722d6c14c6f20116e9d7528
nobu <hiroki-pon@outlook.jp>
2023-12-30 12:15:55 +0900

    firebase_options 関係を修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
+import 'package:app/const/firebase/firebase_dev_options.dart' as dev;
+import 'package:app/const/firebase/firebase_prod_options.dart' as prod;
 import 'package:app/features/auth/screens/myapp/view.dart';
-import 'package:app/firebase_options.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
@@ -9,12 +10,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // ignore: do_not_use_environment
+  const isDev = String.fromEnvironment('flavor') == 'dev';
+
   /// 初期化処理
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: isDev
+        ? dev.DefaultFirebaseOptions.currentPlatform
+        : prod.DefaultFirebaseOptions.currentPlatform,
+  );
 
   unawaited(
     /// 縦画面専用
