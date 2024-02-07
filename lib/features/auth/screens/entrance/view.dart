d43e28b3fcd193a68760dbb1f481e3821aad8e01
nobu74658 <hiroki-pon@outlook.jp>
2024-02-07 10:03:38 +0900

    ボトムナビゲーションを用意

--- lib/features/auth/screens/entrance/view.dart
+++ lib/features/auth/screens/entrance/view.dart
@@ -1,8 +1,47 @@
+import 'package:app/features/auth/screens/sign_up/view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class EntranceScreen extends StatelessWidget {
+class EntranceScreen extends HookWidget {
   const EntranceScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold();
+  Widget build(BuildContext context) {
+    final currentIndex = useState<int>(0);
+    final pages = [
+      const Text('first page'),
+      ElevatedButton(
+        onPressed: () async {
+          await FirebaseAuth.instance.signOut().then(
+                (_) => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (_) => const SignUpScreen()),
+                ),
+              );
+        },
+        child: const Text('log out'),
+      ),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Entrance'),
+      ),
+      body: pages[currentIndex.value],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex.value,
+        onTap: (index) => currentIndex.value = index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
 }
